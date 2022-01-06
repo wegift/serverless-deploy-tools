8a2078936ff7a203e45b5014020775e69212403e
Frederico Marques <fred.marques@wegift.io>
2022-01-06 18:39:32 +0000

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sts-creds',
-    version='0.1.0',
+    version='0.1.1',
     author='WeGift',
     scripts=['bin/sts-creds.py'],
     license='LICENSE.txt',
